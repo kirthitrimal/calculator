0cc5d178be5d51c29cf430cb36e187e2a1b9e82b
kirthitrimal <kirthitrimal@gmail.com>
2026-09-17 15:31:42 +0530

    Implement basic calculator for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+addition = a + b
+subtraction = a - b
+
+print("Addition of votes =", addition)
+print("Subtraction of votes =", subtraction)
