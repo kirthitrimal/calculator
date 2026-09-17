51a91bb88c6dc6ec0fe8714384c26dc506f0bf4a
kirthi <kirthitrimal@gmail.com>
2026-09-17 16:08:32 +0530

    updated operations

--- calculator.py
+++ calculator.py
@@ -1,24 +1,15 @@
 def add(a, b):
     return a + b
-
-
 def subtract(a, b):
     return a - b
-
-
 def multiply(a, b):
     return a * b
-
-
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero.")
     return a / b
-
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-
 print("Addition:", add(num1, num2))
 print("Subtraction:", subtract(num1, num2))
 print("Multiplication:", multiply(num1, num2))
